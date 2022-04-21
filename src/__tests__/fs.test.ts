72fc004c97aeab28a58bc31a5cea7d0550535a8f
envis10n <envis10n@envis10n.dev>
2022-04-21 18:12:10 -0500

    First pass for removeUser.

--- src/__tests__/fs.test.ts
+++ src/__tests__/fs.test.ts
@@ -1,25 +1,23 @@
-import arango, { createFilesystem } from "../database/database.ts";
+import arango, { getFilesystem } from "../database/database.ts";
+import System from "../database/models/system.ts";
+import { createUser, removeUser } from "../user.ts";
 import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
 
 Deno.test("fs-test", async () => {
-  const systems = await arango.collection("systems");
-  const filesystems = await arango.collection("filesystems");
-  const edges = await arango.collection("file_links");
-  await filesystems.truncate();
-  await edges.truncate();
-  await systems.truncate();
-  const fs = await createFilesystem("test1");
-  await systems.create({
-    uuid: crypto.randomUUID(),
-    filesystems: fs.connector.root_id,
-  });
-  const stat = await fs.stat("/home/test1");
+  await removeUser("_test_fs1");
+  const systems = await arango.collection<System>("systems");
+  const user = await createUser("_test_fs1", "_test_fs1");
+  const sys = await systems.findOne({ uuid: user.home_system });
+  if (sys == undefined) throw new Error("No system.");
+  const fs = await getFilesystem(sys._id);
+  const stat = await fs.stat("/home/_test_fs1");
   assertEquals(stat.isDirectory, true);
-  assertEquals(await fs.exists("/home/test1/derp.txt"), false);
-  await fs.write("/home/test1/derp.txt", "derp");
-  let res = await fs.read("/home/test1/derp.txt", { encoding: "utf-8" });
+  assertEquals(await fs.exists("/home/_test_fs1/derp.txt"), false);
+  await fs.write("/home/_test_fs1/derp.txt", "derp");
+  let res = await fs.read("/home/_test_fs1/derp.txt", { encoding: "utf-8" });
   assertEquals(res, "derp");
-  await fs.append("/home/test1/derp.txt", "derp");
-  res = await fs.read("/home/test1/derp.txt", { encoding: "utf-8" });
+  await fs.append("/home/_test_fs1/derp.txt", "derp");
+  res = await fs.read("/home/_test_fs1/derp.txt", { encoding: "utf-8" });
   assertEquals(res, "derpderp");
+  assertEquals(await removeUser("_test_fs1"), true);
 });
--- src/user.ts
+++ src/user.ts
@@ -3,9 +3,54 @@ import { Client } from "./net/common.ts";
 import arango, { createFilesystem } from "./database/database.ts";
 import User, { UserDoc } from "./database/models/user.ts";
 import System from "./database/models/system.ts";
+import FSEntry from "./database/models/fsentry.ts";
 
 const users = await arango.collection<User>("users");
 const systems = await arango.collection<System>("systems");
+const filesystems = await arango.collection<FSEntry>("filesystems");
+
+export async function removeUser(username: string): Promise<boolean> {
+  const user = await users.findOne({ username });
+  if (user == undefined) return false;
+  const system = await systems.findOne({ uuid: user.home_system });
+  if (system == undefined) return false;
+  const fs_id = system.filesystem;
+  const root = await filesystems.findOne({ _id: fs_id });
+  if (root == undefined) return false;
+  const graph = await arango.graph("files");
+  const files =
+    await (await graph.traversal<FSEntry>(fs_id, "OUTBOUND", { limit: 500 }))
+      .collect();
+  for (const file of files) {
+    const f = await filesystems.get(file._key);
+    if (!await f.delete()) return false;
+  }
+  if (!await root.delete()) return false;
+  if (!await system.delete()) return false;
+  if (!await user.delete()) return false;
+  return true;
+}
+
+export async function createUser(
+  username: string,
+  password: string,
+): Promise<UserDoc> {
+  const fs = await createFilesystem(username);
+  const sys = await systems.create({
+    uuid: crypto.randomUUID(),
+    filesystem: fs.connector.root_id,
+  });
+  return await users.create({
+    uuid: crypto.randomUUID(),
+    hash: await bcrypt.hash(password),
+    group: username,
+    username,
+    last_login: new Date().toISOString(),
+    connection_chain: sys.uuid,
+    home_system: sys.uuid,
+    created_at: new Date().toISOString(),
+  });
+}
 
 export async function authenticate(client: Client): Promise<UserDoc> {
   const username = await client.prompt("Username: ");
@@ -17,21 +62,7 @@ export async function authenticate(client: Client): Promise<UserDoc> {
       client.send("Passwords did not match.");
       return await authenticate(client);
     } else {
-      const fs = await createFilesystem(username);
-      const sys = await systems.create({
-        uuid: crypto.randomUUID(),
-        filesystem: fs.connector.root_id,
-      });
-      return await users.create({
-        uuid: crypto.randomUUID(),
-        hash: await bcrypt.hash(password),
-        group: username,
-        username,
-        last_login: new Date().toISOString(),
-        connection_chain: sys.uuid,
-        home_system: sys.uuid,
-        created_at: new Date().toISOString(),
-      });
+      return await createUser(username, password);
     }
   } else {
     const password = await client.prompt("Password: ");
